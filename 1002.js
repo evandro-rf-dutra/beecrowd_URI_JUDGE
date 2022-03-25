0075c7d621dfa1591eaef16e54e7cc923894756a
evandro-rf-dutra <evandrorfdutra@gmail.com>
2022-03-25 12:00:37 -0300

    commit_25_03

--- 1002.js
+++ 1002.js
@@ -1,5 +1,14 @@
-/**
-* testando
-*/
+var input = require('fs').readFileSync('stdin', 'utf8'); // readFileSync('/dev/stdin', 'utf8') no URI o diretório é diferente.
+var lines = input.split('\n');
 
-console.log("ola mundo!");
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+var π = 3.14159;
+var raio = parseFloat(input);
+area = (π * (raio * raio));
+
+console.log("A="+area.toFixed(4));
